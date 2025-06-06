ef88a76580dd1013315182f3cb77a38160d3cdd1
alexander krutov <alexanderkrutov@MacBook-Pro-alexander.local>
2025-06-06 15:33:32 +0300

    Коммит от 6 июня

--- src/components/HeroMain.tsx
+++ src/components/HeroMain.tsx
@@ -3,22 +3,24 @@ import '../styles/HeroMain.css';
 const HeroMain = () => {
   return (
     <section className="hero-main">
-      <div className="hero-main-content">
-        <h1 className="hero-main-title">
-          <span className="underline-wrapper">
-            DataSan
-            <span className="underline-bg"></span>
-          </span>
-          <br />
-          деперсонализация<br />
-          базы данных
-        </h1>
-        <p className="hero-main-text">
-          Обеспечьте безопасность данных: 
-          <br /> получите <strong>бесплатное</strong> пошаговое 
-          <br /> руководство по деперсонализации
-        </p>
-        <a href="#" className="hero-main-button">Получить руководство</a>
+      <div className="hero-bg-inner">
+        <div className="hero-main-content">
+          <h1 className="hero-main-title">
+            <span className="underline-wrapper">
+              DataSan
+              <span className="underline-bg"></span>
+            </span>
+            <br />
+            деперсонализация<br />
+            базы данных
+          </h1>
+          <p className="hero-main-text">
+            Обеспечьте безопасность данных: 
+            <br /> получите <strong>бесплатное</strong> пошаговое 
+            <br /> руководство по деперсонализации
+          </p>
+          <a href="#" className="hero-main-button">Получить руководство</a>
+        </div>
       </div>
     </section>
   );
