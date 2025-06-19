d6949537c4b1014e92ca81985abf3bd948673a7a
alexander krutov <alexanderkrutov@MacBook-Pro-alexander.local>
2025-06-19 17:38:05 +0300

    Коммит от 19  июня , шришты

--- src/components/AdvantagesBlock.tsx
+++ src/components/AdvantagesBlock.tsx
@@ -10,42 +10,50 @@ const AdvantagesBlock = () => {
         <div className="col-12 col-md-6 col-lg-3">
           <div className="adv-card violet">
             <div className="adv-number">01</div>
-            <p>Не требует дополнительной инфраструктуры</p>
+            <div className="adv-text">
+              <p>Не требует дополнительной инфраструктуры</p>
+            </div>
           </div>
         </div>
 
         <div className="col-12 col-md-6 col-lg-3">
           <div className="adv-card yellow">
             <div className="adv-number">02</div>
-            <p>Настраиваемые методы маскирования</p>
+            <div className="adv-text">
+              <p>Настраиваемые методы маскирования</p>
+            </div>
           </div>
         </div>
 
         <div className="col-12 col-md-6 col-lg-3">
           <div className="adv-card blue">
             <div className="adv-number">03</div>
-            <p>Позволяет обезличивать интеграционные тестовые среды</p>
+            <div className="adv-text">
+              <p>Позволяет обезличивать интеграционные тестовые среды</p>
+            </div>
           </div>
         </div>
 
         <div className="col-12 col-md-6 col-lg-3">
           <div className="adv-card light">
             <div className="adv-number">04</div>
-            <p>ИНН, номера счетов и другие поля сохраняют контрольные суммы</p>
+            <div className="adv-text">
+              <p>ИНН, номера счетов и другие поля сохраняют контрольные суммы</p>
+            </div>
           </div>
         </div>
 
         <div className="col-12">
-          <div className="adv-card pink d-flex flex-column flex-lg-row align-items-start justify-content-between">
-            <div className="adv-text">
-              <div className="adv-number">05</div>
-              <p><strong>DataSan</strong> обезличивает<br />1 000 000 строк менее чем за минуту</p>
-            </div>
-            <div className="adv-image mt-4 mt-lg-0 ms-lg-4">
-              <img src={performanceImg} alt="Производительность DataSan" />
-            </div>
-          </div>
-        </div>
+  <div className="adv-card adv-card-performance pink d-flex flex-column flex-lg-row align-items-start justify-content-between">
+    <div className="performance-text d-flex flex-column justify-content-between">
+      <div className="adv-number">05</div>
+      <p><strong>DataSan</strong> обезличивает<br />1 000 000 строк <br />менее чем за минуту</p>
+    </div>
+    <div className="adv-image mt-4 mt-lg-0">
+      <img src={performanceImg} alt="Производительность DataSan" />
+    </div>
+  </div>
+</div>
 
       </div>
     </section>
